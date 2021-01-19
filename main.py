416347a9987f10a6350db9b3534bcf1d2781a066
Josh Stow <joshxstow@gmail.com>
2021-01-19 18:05:15 +0000

    Import dataset as list from csv

--- main.py
+++ main.py
@@ -5,3 +5,30 @@ Created on Tue Jan 19 12:31:59 2021
 @author: Josh Stow
 """
 
+# Import dependencies
+import csv
+
+def importData():
+    """
+    Read data from csv into list.
+    
+    Args:
+        None
+        
+    Returns:
+        data::list
+
+    """
+    with open('dataset.csv', 'r') as f:
+        reader = csv.reader(f)  # Instantiate csv reader obj
+        data = [row for row in reader]  # Comprehend list of rows from csv
+        del data[0] # Remove header row
+        
+        return data
+
+# Run code if file run from command line
+if __name__ == '__main__':
+    DATASET = importData()
+    print(data)
+        
+    
