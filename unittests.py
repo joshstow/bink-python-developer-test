4b753d7bb20efcd5d87f27fe5c13d79491af544d
Josh Stow <joshxstow@gmail.com>
2021-01-20 01:09:08 +0000

    Created unittests.py

---
+++ unittests.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 20 01:03:10 2021
+
+@author: Josh Stow
+"""
+
+# Import dependencies
+import unittest
+import main
+
+# Create class inheriting from TestCase
+class Test(unittest.TestCase):
+    
+    def test1(self):
+        pass
+
+# Run code if file run from command line
+if __name__ == '__main__':
+    unittest.main()
